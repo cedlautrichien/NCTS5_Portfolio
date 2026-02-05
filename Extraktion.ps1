20755bac4a74e6fd5dfef2e8623d0efa062f41a5
Cedric <98392989+cedlautrichien@users.noreply.github.com>
2026-02-05 10:31:06 +0100

    Delete Extraktion.ps1

--- Extraktion.ps1
+++
@@ -1,9 +0,0 @@
-Remove-Item D:\Daten\XMLFabrik\Variabeln.txt
-$template = Get-Content 'D:\Daten\XMLFabrik\CC015C_E2E-A-T1-S2-max-1.xml'
-$pattern = '(?<=\$).*?(?=\$)'
-
-ForEach ($line in $template) { 
-    if ( $line -match $pattern ) {
-        $Matches[0] | Out-File -FilePath 'D:\Daten\XMLFabrik\Variabeln.txt' -Append
-    }
-}
